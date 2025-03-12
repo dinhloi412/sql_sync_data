358fb94ec849ff2b5c8a2c0342b219af2ecb4e20
Frank-Nguyen <dinhloi412@gmail.com>
2025-03-12 11:04:46 +0700

    feat: refactor code

--- models/phieucan.py
+++ models/phieucan.py
@@ -43,13 +43,13 @@ class SyncWeightman(models.Model):
                 'error': "Thiếu Ticketnum"
             }
 
-        savepoint_name = "sync_weightman_save"
+        # savepoint_name = "sync_weightman_save"
         try:
-            self.env.cr.execute("SAVEPOINT %s" % savepoint_name)
+            # self.env.cr.execute("SAVEPOINT %s" % savepoint_name)
             self.env.cr.execute("""
                 SELECT id FROM weightman 
-                WHERE ticket_num = %s
-            """, (values.get('Ticketnum'),))
+                WHERE ticket_num = %s and warehouse_id = %s
+            """, (values.get('Ticketnum'), values.get('agent_name'),))
 
             existing_record = self.env.cr.fetchone()
             common_values = self._prepare_values(values)
@@ -85,14 +85,10 @@ class SyncWeightman(models.Model):
                 """
                 params = common_values + (values.get('Ticketnum'),)
                 self.env.cr.execute(update_query, params)
-                updated_id = self.env.cr.fetchone()
-
-                self.env.cr.execute("RELEASE SAVEPOINT %s" % savepoint_name)
+                # self.env.cr.execute("RELEASE SAVEPOINT %s" % savepoint_name)
 
                 return {
                     'success': True,
-                    'message': f"Đã cập nhật bản ghi {values.get('Ticketnum')}",
-                    'id': updated_id[0] if updated_id else existing_record[0]
                 }
             else:
                 insert_query = """
@@ -113,8 +109,7 @@ class SyncWeightman(models.Model):
                 """
                 params = (SUPERUSER_ID, SUPERUSER_ID, values.get('Ticketnum')) + common_values
                 self.env.cr.execute(insert_query, params)
-
-                self.env.cr.execute("RELEASE SAVEPOINT %s" % savepoint_name)
+                # self.env.cr.execute("RELEASE SAVEPOINT %s" % savepoint_name)
 
                 return {
                     'success': True,
@@ -122,7 +117,7 @@ class SyncWeightman(models.Model):
 
         except Exception as e:
             _logger.exception(f"Lỗi khi xử lý bản ghi: {str(e)}")
-            self.env.cr.execute("ROLLBACK TO SAVEPOINT %s" % savepoint_name)
+            # self.env.cr.execute("ROLLBACK TO SAVEPOINT %s" % savepoint_name)
             return {
                 'success': False,
                 'error': str(e)
